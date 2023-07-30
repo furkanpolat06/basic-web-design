511b841818219a84ab0fa550f139b4e1c25684b9
furkanpolat06 <emnplt.0698@gmail.com>
2023-07-30 06:46:54 +0300

    fizyoweb design

---
+++ script.js
@@ -0,0 +1,30 @@
+// const word = "hello word";
+// let x = "";
+// function reverseLetter(word){for (let i = word.length - 1; i >= 0; i--) {
+//   x += word[i];
+//   console.log(x);
+// }
+// }
+// const word = "hello word";
+
+// function reverseLetter(word) {
+//   let x = "";
+//   for (let i = word.length - 1; i >= 0; i--) {
+//     x += word[i];
+    
+//   }
+//   return x;
+// }
+
+// console.log(reverseLetter(word));
+
+let words=("hello")
+let x=""
+for(let i=words.length-1; i>=0; i--){
+  x+=words[i];
+ 
+}
+console.log((x));
+
+// let x=prompt("lütfen bir cümle giriniz")
+// console.log((x.split(' ').reverse()).join(" "));
